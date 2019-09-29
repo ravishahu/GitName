02f2749be9b230343fb0f4c44fb497a2065ee172
ADS\T1048279 <shashuputta.ravikiran@gmail.com>
2019-09-29 17:28:38 +0100

    FirstCommit
    test

---
+++ GitName/Controllers/GitUserController.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Security.Authentication;
+using System.Web.Mvc;
+using GitName.Models;
+
+namespace GitName.Controllers
+{
+    public class GitUserController : Controller
+    {
+        private MyJSon MyJsonObject { get; set; }
+        // GET: GitUser
+        public ActionResult Index(string searchString)
+        {
+            if (!string.IsNullOrEmpty(searchString))
+            GetGitUser("https://api.github.com/users/" + searchString);
+            return View(MyJsonObject);
+        }
+
+       private void GetGitUser(string url)
+        {
+            const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
+            const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
+            ServicePointManager.SecurityProtocol = Tls12;
+
+            HttpWebRequest webRequest = WebRequest.Create(url) as HttpWebRequest;
+            if (webRequest != null)
+            {
+                webRequest.Method = "GET";
+                webRequest.UserAgent = "Anything";
+                webRequest.ServicePoint.Expect100Continue = false;
+
+                try
+                {
+                    using (StreamReader responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream()))
+                    {
+                        string reader = responseReader.ReadToEnd();
+                        MyJsonObject = JsonConvert.DeserializeObject<MyJSon>(reader);
+                    }
+                }
+                catch(Exception e)
+                {
+                    throw e;
+                }
+            }
+            
+        }
+    }
+}
